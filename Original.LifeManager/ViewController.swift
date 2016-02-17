b7ab1d28c110b3af8e97daf7fed3e847d1bca0ca
Aoi Sakaue <aoiue.w.bbb@gmail.com>
2016-02-17 16:51:57 +0900

    Initial Commit

---
+++ Original.LifeManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Original.LifeManager
+//
+//  Created by Aoi Sakaue on 2016/02/17.
+//  Copyright © 2016年 Aoi Sakaue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Original.LifeManagerTests/Original_LifeManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Original_LifeManagerTests.swift
+//  Original.LifeManagerTests
+//
+//  Created by Aoi Sakaue on 2016/02/17.
+//  Copyright © 2016年 Aoi Sakaue. All rights reserved.
+//
+
+import XCTest
+@testable import Original_LifeManager
+
+class Original_LifeManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
